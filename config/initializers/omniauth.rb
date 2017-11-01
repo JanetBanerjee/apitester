90e57eee739e74fde4a88a42834ba28a7ae93cb9
Janet Banerjee <banerjee.janet@gmail.com>
2017-11-01 18:05:17 -0400

    Facebook attempt #2 working, SSL error, installed Faraday

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '185822845299749', '9dcd5599fff24bacd185296c54f4971a'
+  provider :facebook, '185822845299749', '9dcd5599fff24bacd185296c54f4971a', {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
 end
